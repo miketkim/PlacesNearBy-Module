3c7dc9ecbc5c2934eb8c6a843a3a223e299c9691
Michael Kim <miketkim@umich.edu>
2019-08-27 17:10:30 -0700

    carousel rendered

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -37,7 +37,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <PlacesNearby places={this.state.places} />
+        <PlacesNearby places ={this.state.places}/>
       </div>
     );
   }
